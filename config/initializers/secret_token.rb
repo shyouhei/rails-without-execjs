f68846134ea735430818ca3d557ef934f74e0481
Urabe, Shyouhei <shyouhei@ruby-lang.org>
2012-06-07 06:11:00 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsWithoutExecjs::Application.config.secret_token = '731f511fca8a9aaf2f505fd8cc1379e7e0f16accafc0a24cdb8a8d400496312b0b54db38ccdfba3ce401f54f2c809826d246622fbe92d3c002bbc5d039da90dd'
